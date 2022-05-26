32850d494fc5e49fa5b3bed403d6aedab89d8a53
Enrique Jimenez <enrique.jimenez@bosonit.com>
2022-05-26 11:22:55 +0200

    Rama main luchando

---
+++ src/main/java/formacion/Perfiles/Bs41PerfilesApplication.java
@@ -0,0 +1,13 @@
+package formacion.Perfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bs41PerfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bs41PerfilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/formacion/Perfiles/Bs41PerfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package formacion.Perfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bs41PerfilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
